dd8da5e0d89bae0dcb78660f7f1c362de95ef176
Nicolas LABEY <nicolas@weda.fr>
2018-12-28 17:15:51 +0100

    feat(photo): add a task to move all the raw files of a folder into a subfolder called raw.

--- KPhotoTools/MainWindow.xaml.cs
+++ KPhotoTools/MainWindow.xaml.cs
@@ -33,25 +33,36 @@ namespace KPhotoTools
 
         private async void Button_StartTask_ClickAsync(object sender, RoutedEventArgs e)
         {
-            if (RadioButton_DeleteRawFromJpg.IsChecked.HasValue && RadioButton_DeleteRawFromJpg.IsChecked.Value)
+            IsProcessingTask = true;
+            try
             {
-                IsProcessingTask = true;
-                TextBlock_Log.Text = "Start deleting raw from jpg";
-                try
+                // Delete raw from jpg
+                if (RadioButton_DeleteRawFromJpg.IsChecked.HasValue && RadioButton_DeleteRawFromJpg.IsChecked.Value)
                 {
+                    TextBlock_Log.Text = "Start deleting raw from jpg";
                     await DeleteRawFromJpg.Start(
                         TextBox_WorkingDirectory.Text.Trim(),
                         TextBox_JpgExtension.Text.Trim(),
                         TextBox_RawExtension.Text.Trim()
                     );
                 }
-                catch (Exception ex)
+                // Move raws to a subfolder
+                else if (RadioButton_MoveRaws.IsChecked.HasValue && RadioButton_MoveRaws.IsChecked.Value)
                 {
-                    TextBlock_Log.Text += "\r\n" + ex.Message;
+                    TextBlock_Log.Text = "Start moving raws in a subdirectory";
+                    await IsolateRawFiles.Start(
+                        TextBox_WorkingDirectory.Text.Trim(),
+                        TextBox_RawExtension.Text.Trim()
+                    );
                 }
-                IsProcessingTask = false;
-                TextBlock_Log.Text += "\r\nFinished";
             }
+            catch (Exception ex)
+            {
+                TextBlock_Log.Text += "\r\n" + ex.Message;
+            }
+
+            IsProcessingTask = false;
+            TextBlock_Log.Text += "\r\nFinished";
         }
     }
 }
---
+++ KPhotoToolsLibrary/IsolateRawFiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPhotoToolsLibrary
+{
+    public class IsolateRawFiles
+    {
+        public static async Task Start(string directoryPath, string rawExtension)
+        {
+            DirectoryInfo workingDir = new DirectoryInfo(directoryPath);
+            if (!workingDir.Exists)
+            {
+                throw new Exception("The directory does not exists: " + directoryPath);
+            }
+
+            DirectoryInfo rawDirectory = new DirectoryInfo(Path.Combine(workingDir.FullName, "raw"));
+            if (rawDirectory.Exists)
+            {
+                throw new Exception("the raw directory already exists, aborting");
+            }
+
+            rawDirectory.Create();
+
+            foreach (FileInfo fileInfo in workingDir.GetFiles("*." + rawExtension.Replace(".", "")))
+            {
+                fileInfo.MoveTo(Path.Combine(rawDirectory.FullName, fileInfo.Name));
+            }
+        }
+    }
+}
